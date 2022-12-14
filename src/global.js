349b89a815bf81701d2ccf434089d21c31f43f31
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-12-14 20:23:12 +0530

    added rendor deployed backend url

--- src/global.js
+++ src/global.js
@@ -1,3 +1,6 @@
 // export const API = "http://localhost:4000";
 
-export const API = "https://car-rentals-backend-raghu.herokuapp.com";
+// export const API = "https://car-rentals-backend-raghu.herokuapp.com";
+
+
+export const API = "https://car-rentals-backend-wdqc.onrender.com";
